bb89574b79dd6c55beeb557f1e7a2a008268fe63
Toby Crawley <toby@tcrawley.org>
2015-06-03 08:23:01 -0400

    Initial commit.

---
+++ src/main/java/org/projectodd/wunderboss/messaging/artemis/ArtemisMessaging.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2015 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.projectodd.wunderboss.messaging.artemis;
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
+import org.apache.activemq.artemis.api.jms.JMSFactoryType;
+import org.apache.activemq.artemis.core.remoting.impl.invm.InVMConnectorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+import org.apache.activemq.artemis.jms.server.JMSServerManager;
+import org.apache.activemq.artemis.jms.server.embedded.EmbeddedJMS;
+import org.apache.activemq.artemis.spi.core.naming.BindingRegistry;
+import org.projectodd.wunderboss.Options;
+import org.projectodd.wunderboss.WunderBoss;
+import org.projectodd.wunderboss.messaging.jms2.JMSDestination;
+import org.projectodd.wunderboss.messaging.jms2.JMSMessaging;
+import org.projectodd.wunderboss.messaging.jms2.JMSQueue;
+import org.projectodd.wunderboss.messaging.jms2.JMSTopic;
+import org.slf4j.Logger;
+
+import javax.jms.ConnectionFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class ArtemisMessaging extends JMSMessaging {
+
+    public ArtemisMessaging(String name, Options<CreateOption> options) {
+        this.name = name;
+        this.options = options;
+    }
+
+    @Override
+    public synchronized void start() throws Exception {
+        if (!started) {
+            this.server = new EmbeddedJMS();
+
+            ClassLoader cl = this.getClass().getClassLoader();
+            if (cl.getResource("broker.xml") == null) {
+                this.server.setConfigResourcePath("default-broker.xml");
+            }
+
+            this.server.start();
+            this.started = true;
+
+            TransportConfiguration config = new TransportConfiguration(InVMConnectorFactory.class.getName());
+            BindingRegistry registry = jmsServerManager().getRegistry();
+
+            registry.bind(JNDI_CF_NAME,
+                          ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, config));
+            registry.bind(JNDI_XA_CF_NAME,
+                          ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.XA_CF, config));
+        }
+    }
+
+    @Override
+    public synchronized void stop() throws Exception {
+        if (started) {
+            closeCloseables();
+            this.server.stop();
+            this.server = null;
+            this.started = false;
+        }
+    }
+
+    @Override
+    public synchronized boolean isRunning() {
+        return started;
+    }
+
+    public JMSServerManager jmsServerManager() {
+        if (this.started) {
+            return this.server.getJMSServerManager();
+        }
+
+        return null;
+    }
+
+    @Override
+    public String name() {
+        return this.name;
+    }
+
+    protected ConnectionFactory createRemoteConnectionFactory(final Options<CreateContextOption> options) {
+        TransportConfiguration config =
+                new TransportConfiguration(NettyConnectorFactory.class.getName(),
+                        new HashMap() {{
+                            put("host", options.getString(CreateContextOption.HOST));
+                            put("port", options.getInt(CreateContextOption.PORT));
+                        }});
+        ActiveMQConnectionFactory cf =
+                ActiveMQJMSClient.createConnectionFactoryWithoutHA(options.has(CreateContextOption.XA) ?
+                                                                           JMSFactoryType.XA_CF :
+                                                                           JMSFactoryType.CF,
+                                                                   config);
+
+        cf.setReconnectAttempts(options.getInt(CreateContextOption.RECONNECT_ATTEMPTS));
+        cf.setRetryInterval(options.getLong(CreateContextOption.RECONNECT_RETRY_INTERVAL));
+        cf.setRetryIntervalMultiplier(options.getDouble(CreateContextOption.RECONNECT_RETRY_INTERVAL_MULTIPLIER));
+        cf.setMaxRetryInterval(options.getLong(CreateContextOption.RECONNECT_MAX_RETRY_INTERVAL));
+
+        return cf;
+    }
+
+    protected javax.jms.Topic createTopic(String name) throws Exception {
+        this.server
+                .getJMSServerManager()
+                .createTopic(false, name, JMSDestination.jndiName(name, "topic"));
+
+        return lookupTopic(name);
+    }
+
+    protected javax.jms.Queue createQueue(String name, String selector, boolean durable) throws Exception {
+        this.server
+                .getJMSServerManager()
+                .createQueue(false, name, selector, durable, JMSDestination.jndiName(name, "queue"));
+
+        return lookupQueue(name);
+    }
+
+    protected javax.jms.Topic lookupTopic(String name) {
+        List<String> jndiNames = new ArrayList<>();
+
+        if (this.server != null) {
+            jndiNames.addAll(Arrays.asList(this.server.getJMSServerManager().getBindingsOnTopic(name)));
+        }
+        jndiNames.add(name);
+        jndiNames.add(JMSTopic.jmsName(name));
+        jndiNames.add(JMSDestination.jndiName(name, "topic"));
+
+        return (javax.jms.Topic)lookupJNDI(jndiNames);
+    }
+
+    protected javax.jms.Queue lookupQueue(String name) {
+        List<String> jndiNames = new ArrayList<>();
+
+        if (this.server != null) {
+            jndiNames.addAll(Arrays.asList(this.server.getJMSServerManager().getBindingsOnQueue(name)));
+        }
+        jndiNames.add(name);
+        jndiNames.add(JMSQueue.jmsName(name));
+        jndiNames.add(JMSDestination.jndiName(name, "queue"));
+
+        return (javax.jms.Queue)lookupJNDI(jndiNames);
+    }
+
+    protected void destroyQueue(String name) throws Exception {
+        this.jmsServerManager().destroyQueue(name, true);
+    }
+
+    protected void destroyTopic(String name) throws Exception {
+        this.jmsServerManager().destroyTopic(name, true);
+    }
+
+    protected Object lookupJNDI(String jndiName) {
+        return server.getRegistry().lookup(jndiName);
+    }
+
+    private final String name;
+    private final Options<CreateOption> options;
+    private boolean started = false;
+    private EmbeddedJMS server;
+
+    private final static Logger log = WunderBoss.logger("org.projectodd.wunderboss.messaging.artemis");
+}
---
+++ src/test/clojure/wunderboss/messaging_tests.clj
@@ -0,0 +1,334 @@
+;; Copyright 2015 Red Hat, Inc, and individual contributors.
+;;
+;; Licensed under the Apache License, Version 2.0 (the "License");
+;; you may not use this file except in compliance with the License.
+;; You may obtain a copy of the License at
+;;
+;; http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing, software
+;; distributed under the License is distributed on an "AS IS" BASIS,
+;; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+;; See the License for the specific language governing permissions and
+;; limitations under the License.
+
+(ns wunderboss.messaging-tests
+  (:require [clojure.test :refer :all])
+  (:import [org.projectodd.wunderboss Option WunderBoss]
+           [org.projectodd.wunderboss.codecs Codecs None StringCodec]
+           [org.projectodd.wunderboss.messaging Messaging
+            Context Context$Mode
+            ConcreteReply
+            Destination Destination$ListenOption Destination$ReceiveOption
+            Destination$PublishOption
+            Queue
+            Queue$RespondOption
+            Topic$SubscribeOption Topic$UnsubscribeOption
+            Messaging$CreateContextOption Messaging$CreateQueueOption
+            MessageHandler]
+           java.util.concurrent.TimeUnit))
+
+(def default (doto (WunderBoss/findOrCreateComponent Messaging) (.start)))
+
+(defn create-opts-fn [class]
+  (let [avail-options (->> class
+                        Option/optsFor
+                        (map #(vector (keyword (.name %)) %))
+                        (into {}))]
+    (fn [opts]
+      (reduce (fn [m [k v]]
+                (assoc m
+                  (if-let [enum (avail-options k)]
+                    enum
+                    (throw (IllegalArgumentException. (str k " is not a valid option."))))
+                  v))
+        {}
+        opts))))
+
+(def coerce-queue-options (create-opts-fn Messaging$CreateQueueOption))
+(def coerce-context-options (create-opts-fn Messaging$CreateContextOption))
+(def coerce-listen-options (create-opts-fn Destination$ListenOption))
+(def coerce-respond-options (create-opts-fn Queue$RespondOption))
+(def coerce-receive-options (create-opts-fn Destination$ReceiveOption))
+(def coerce-publish-options (create-opts-fn Destination$PublishOption))
+(def coerce-subscribe-options (create-opts-fn Topic$SubscribeOption))
+(def coerce-unsubscribe-options (create-opts-fn Topic$UnsubscribeOption))
+
+(def frob-codec
+  (proxy [StringCodec] ["frob" "application/frob"]
+    (encode [ data]
+      (format "FROBBED %s" data))
+    (decode [data]
+      (format "DEFROBBED %s" data))))
+
+(def codecs (-> (Codecs.) (.add None/INSTANCE) (.add frob-codec)))
+
+(defn handler [f]
+  (reify MessageHandler
+    (onMessage [_ m _]
+      (ConcreteReply. (f (.body m)) nil))))
+
+(defn create-queue [& opts]
+  (let [[name opts] opts
+        name (or name (str (java.util.UUID/randomUUID)))]
+    (.findOrCreateQueue default name
+      (coerce-queue-options (if (:context opts)
+                              opts
+                              (merge opts {:durable false}))))))
+
+(defn create-topic [name]
+  (.findOrCreateTopic default name nil))
+
+(deftest queue-creation-publish-receive-close
+  (let [queue (create-queue "a-queue")]
+
+    ;; findOrCreateQueue should return the same queue for the same name
+    (is (= (.jmsDestination queue)
+          (.jmsDestination (create-queue "a-queue"))))
+
+    ;; we should be able to publish and rcv
+    (.publish queue "hi" None/INSTANCE nil)
+    (let [msg (.receive queue codecs (coerce-receive-options {:timeout 1000}))]
+      (is msg)
+      (is (= "hi" (.body msg))))
+
+    ;; a stopped queue should no longer be avaiable
+    (.stop queue)
+    (is (thrown? javax.jms.InvalidDestinationRuntimeException
+          (.receive queue codecs (coerce-receive-options {:timeout 1}))))))
+
+(deftest publish-should-use-the-passed-context
+  (let [c (.createContext default nil)
+        q (create-queue "publish-c")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.publish q "boom" None/INSTANCE
+            (coerce-publish-options {:context c}))))))
+
+(deftest publish-should-encode-with-the-given-codec-and-receive-should-find-the-right-one
+  (let [q (create-queue)]
+    (.publish q "hi" frob-codec nil)
+    (is (= "DEFROBBED FROBBED hi" (.body (.receive q codecs nil))))))
+
+(deftest request-should-use-the-passed-context
+  (let [c (.createContext default (coerce-context-options {:host "localhost"}))
+        q (create-queue "publish-c")]
+    (.close c)
+    (try
+      (.request q "boom" None/INSTANCE codecs
+        (coerce-publish-options {:context c}))
+      (catch Exception e
+        (is (instance? javax.jms.IllegalStateRuntimeException (.getCause e)))))))
+
+(deftest receive-should-use-the-passed-context
+  (let [c (.createContext default nil)
+        q (create-queue "receive-context")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.receive q codecs (coerce-receive-options {:context c}))))))
+
+(deftest listen-should-use-the-passed-context
+  (let [c (.createContext default (coerce-context-options {:host "localhost"}))
+        q (create-queue "listen-context")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.listen q (handler identity) codecs (coerce-listen-options {:context c}))))))
+
+(deftest respond-should-use-the-passed-context
+  (let [c (.createContext default (coerce-context-options {:host "localhost"}))
+        q (create-queue "listen-context")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.respond q (handler identity) codecs (coerce-listen-options {:context c}))))))
+
+(deftest subscribe-should-use-the-passed-context
+  (let [c (.createContext default (coerce-context-options {:client_id "ham"}))
+        t (create-topic "subscribe-context")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.subscribe t "ham" (handler identity) codecs (coerce-subscribe-options {:context c}))))))
+
+(deftest unsubscribe-should-use-the-passed-context
+  (let [c (.createContext default (coerce-context-options {:client_id "ham"}))
+        t (create-topic "subscribe-context")]
+    (.close c)
+    (is (thrown? javax.jms.IllegalStateRuntimeException
+          (.unsubscribe t "ham" (coerce-unsubscribe-options {:context c}))))))
+
+(deftest closing-a-listener-should-work
+  (let [queue (create-queue "listen-queue")]
+    (let [called (atom (promise))
+          listener (.listen queue
+                     (handler
+                       (fn [msg]
+                         (deliver @called msg)))
+                     codecs
+                     nil)]
+      (.publish queue "hi" None/INSTANCE nil)
+      (is (= "hi" (deref @called 1000 :failure)))
+      (reset! called (promise))
+      (.close listener)
+      (.publish queue "hi" None/INSTANCE nil)
+      (is (= :success (deref @called 1000 :success))))))
+
+(deftest subscribe-to-topic
+  (let [topic (create-topic "subscribe-topic")
+        called (atom (promise))
+        listener (.subscribe topic
+                   "my-sub"
+                   (handler
+                     (fn [msg]
+                       (deliver @called msg)))
+                   codecs
+                   nil)]
+    (.publish topic "hi" None/INSTANCE nil)
+    (is (= "hi" (deref @called 1000 :failure)))
+    (reset! called (promise))
+    (.close listener)
+    (.publish topic "hi-again" None/INSTANCE nil)
+    (is (= :failure (deref @called 100 :failure)))
+    (with-open [listener (.subscribe topic
+                           "my-sub"
+                           (handler
+                             (fn [msg]
+                               (deliver @called msg)))
+                           codecs
+                           nil)]
+      (is (= "hi-again" (deref @called 1000 :failure))))
+    (.unsubscribe topic "my-sub" nil)))
+
+(deftest unsubscribe-from-topic
+  (let [topic (create-topic "subscribe-topic")
+        called (atom (promise))
+        listener (.subscribe topic
+                   "another-sub"
+                   (handler
+                     (fn [msg]
+                       (deliver @called msg)))
+                   codecs
+                   nil)]
+    (.publish topic "hi" None/INSTANCE nil)
+    (is (= "hi" (deref @called 1000 :failure)))
+    (reset! called (promise))
+    (.close listener)
+    (.unsubscribe topic "another-sub" nil)
+    (.publish topic "failure" None/INSTANCE nil)
+    (is (= :success (deref @called 100 :success)))
+    (with-open [listener (.subscribe topic
+                           "another-sub"
+                           (handler
+                             (fn [msg]
+                               (deliver @called msg)))
+                           codecs
+                           nil)]
+      (is (= :success (deref @called 100 :success))))))
+
+(deftest listen-with-concurrency
+  (let [queue (create-queue "listen-queue")
+        latch (java.util.concurrent.CountDownLatch. 5)
+        listener (.listen queue
+                   (handler
+                     (fn [msg]
+                       (let [msg (read-string msg)]
+                         (.countDown latch)
+                         (.await latch))))
+                   codecs
+                   (coerce-listen-options {:concurrency 5}))]
+    (dotimes [n 5]
+      (.publish queue (str n) None/INSTANCE nil))
+    (is (.await latch 10 TimeUnit/SECONDS))
+    (.close listener)))
+
+(deftest request-response
+  (let [queue (create-queue "rr-queue")]
+    (with-open [listener (.respond queue
+                           (handler identity)
+                           codecs
+                           nil)]
+      (let [response (.request queue "hi" None/INSTANCE codecs nil)]
+        (is (= "hi" (.body (.get response))))
+        ;; result should be cached
+        (is (= "hi" (.body (.get response))))))))
+
+(deftest request-response-should-coordinate-requests-with-responses
+  (let [queue (create-queue "rr-coord-queue")]
+    (with-open [listener (.respond queue
+                           (handler
+                             (fn [msg]
+                               (let [time (read-string msg)]
+                                 (Thread/sleep time)
+                                 (str "response-" time))))
+                           codecs
+                           (coerce-respond-options {:concurrency 5}))]
+      (let [response1 (.request queue "50" None/INSTANCE codecs nil)
+            response2 (.request queue "100" None/INSTANCE codecs nil)
+            response3 (.request queue "25" None/INSTANCE codecs nil)]
+        (is (= "response-50" (.body (.get response1))))
+        (is (= "response-100" (.body (.get response2))))
+        (is (= "response-25" (.body (.get response3))))))))
+
+(deftest request-response-with-ttl
+  (let [queue (create-queue "rr-queue")
+        latch (java.util.concurrent.CountDownLatch. 1)]
+    (with-open [listener (.respond queue
+                           (handler (fn [m]
+                                      (.countDown latch)
+                                      m))
+                           codecs
+                           (coerce-respond-options {:ttl 1}))]
+      (let [response (.request queue "nope" None/INSTANCE codecs nil)]
+        (.await latch 10 TimeUnit/SECONDS)
+        (Thread/sleep 100)
+        (is (thrown? java.util.concurrent.TimeoutException
+              (.get response 1 TimeUnit/MILLISECONDS)))))))
+
+(deftest context-rollback
+  (let [c (.createContext default
+            (coerce-context-options {:mode Context$Mode/TRANSACTED}))
+        q (create-queue "rollback")]
+    (.publish q "failure" None/INSTANCE
+      (coerce-publish-options {:context c}))
+    (.rollback c)
+    (is (not (.receive q codecs (coerce-receive-options {:timeout 1000}))))
+    (.close c)))
+
+(deftest remote-contexts
+  ;; this creates the queue in the 'remote' broker
+  (create-queue "remote-queue")
+  (with-open [c (.createContext default
+                  (coerce-context-options {:host "localhost"}))]
+    (let [q (create-queue "remote-queue" {:context c})]
+      (.publish q "success" None/INSTANCE
+        (coerce-publish-options {:context c}))
+      (let [msg (.receive q codecs (coerce-receive-options {:context c
+                                                            :timeout 1000}))]
+        (is msg)
+        (is (= "success" (.body msg)))))))
+
+(deftest publish-receive-with-the-same-context-should-work
+  (with-open [ctx (.createContext default nil)]
+    (let [q (create-queue)]
+      (.publish q "success" None/INSTANCE (coerce-publish-options {:context ctx}))
+      (is (= "success"
+            (.body (.receive q codecs (coerce-receive-options {:context ctx}))))))))
+
+(deftest publish-and-receive-inside-a-non-transactional-listener-should-work
+  (let [q1 (create-queue)
+        q2 (create-queue)
+        p  (promise)
+        l  (.listen q1
+             (handler
+               (fn [msg]
+                 (.publish q2 msg None/INSTANCE nil)
+                 (when-let [result (.receive q2 codecs (coerce-receive-options {:timeout 1000}))]
+                   (is (= msg (.body result)))
+                   (deliver p (.body result)))))
+             codecs
+             (coerce-listen-options {:mode Context$Mode/AUTO_ACK}))]
+    (.publish q1 "whatevs" None/INSTANCE nil)
+    (is (= "whatevs" (deref p 1000 :failure)))))
+
+(deftest toss-when-xa-unavailable
+  (is (thrown-with-msg? NullPointerException
+        #"TransactionManager not found"
+        (.createContext default (coerce-context-options {:xa true})))))
